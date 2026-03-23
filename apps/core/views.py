e3bd8fd58e23ddb52edffd209957d32a147834d6
gohil-gaurav <gohilgaurav89@gmail.com>
2026-03-23 17:24:14 +0530

    Implement hero section redesign and add latest products display; introduce new styles for enhanced UI

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,12 @@
 from django.views.generic import TemplateView
 
+from apps.products.models import Product
+
 
 class HomeView(TemplateView):
 	template_name = "core/home.html"
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context["latest_products"] = Product.objects.filter(is_active=True)[:8]
+		return context
